c8d103fa091bfdb07d0877984bec2eb67223d05a
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-11 22:59:50 -0300

    Testing for messages receipt and sending.

--- test/messenger-tests.js
+++ test/messenger-tests.js
@@ -15,4 +15,24 @@ describe('Messenger tests', function () {
 
     done()
   })
+
+  it('should receive a message', function (done) {
+    let msg = new Messenger()
+    msg.Receive('foo', new Date(), 'lorem ipsum')
+
+    expect(msg.CountReceived()).to.be(1)
+    expect(msg.CountSent()).to.be(0)
+
+    done()
+  })
+
+  it('should send a message', function (done) {
+    let msg = new Messenger()
+    msg.Send('foo', new Date(), 'lorem ipsum')
+
+    expect(msg.CountSent()).to.be(1)
+    expect(msg.CountReceived()).to.be(0)
+
+    done()
+  })
 })
