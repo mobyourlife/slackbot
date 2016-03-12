f5b3eafadc19bf3f7c2d759a405863323b7f619c
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-12 00:04:15 -0300

    Playing with the Slack client.

---
+++ bin/service.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Messenger = require('../lib/messenger')
+
+const slack = require('@slack/client')
+const RtmClient = slack.RtmClient
+const RTM_EVENTS = slack.RTM_EVENTS
+
+const token = process.env.SLACK_TOKEN
+
+const msg = new Messenger()
+
+const rtm = new RtmClient(token, {
+  //logLevel: 'debug'
+})
+
+rtm.on(RTM_EVENTS.MESSAGE, (data) => {
+  console.log(data)
+  msg.Receive(data.user, data.ts, data.text)
+})
+
+rtm.start()
