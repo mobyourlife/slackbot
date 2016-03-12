4fcf3178935c42e64911a7ec71ddde3123dc58e7
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-11 22:25:55 -0300

    Taking messages from the repository.

--- test/repository.js
+++ test/repository.js
@@ -15,11 +15,44 @@ describe('Repository tests', function () {
     done()
   })
 
-  it('should add one message to the repository', function (done) {
+  it('should add a message to the repository', function (done) {
     let repo = new Repository()
-    repo.Push('foo', Date.now(), 'lorem ipsum')
+    repo.Push('foo', new Date(), 'lorem ipsum')
 
     expect(repo.Count()).to.be(1)
+
+    done()
+  })
+
+  it('should not take anything from an empty repository', function (done) {
+    let repo = new Repository()
+    let item = repo.Pop()
+
+    expect(repo.Count()).to.be(0)
+    expect(item).not.to.be.ok()
+
+    done()
+  })
+
+  it('should take the added message from the repository', function (done) {
+    let data = {
+      username: 'foo',
+      date: Date.now(),
+      message: 'lorem ipsum'
+    }
+
+    let repo = new Repository()
+    repo.Push(data.username, data.date, data.message)
+
+    let item = repo.Pop()
+
+    expect(repo.Count()).to.be(1)
+
+    expect(item).to.be.ok()
+    // expect(item).to.be.a(Repository)
+    expect(item.username).to.be(data.username)
+    expect(item.date).to.be(data.date)
+    expect(item.message).to.be(data.message)
 
     done()
   })
