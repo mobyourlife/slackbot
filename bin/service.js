223a08559e951e45b8ea81a93012ad00c39ec627
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-12 14:58:57 -0300

    Respondendo as mensagens para o Slack bot.

--- bin/service.js
+++ bin/service.js
@@ -1,22 +1,25 @@
 'use strict'
 
-const Messenger = require('../lib/messenger')
+// inicializa o proto bot
+const ProtoBot = require('../lib/protobot')
+var bot = new ProtoBot()
 
+// carrega o módulo do slack bot
 const slack = require('@slack/client')
 const RtmClient = slack.RtmClient
 const RTM_EVENTS = slack.RTM_EVENTS
-
 const token = process.env.SLACK_TOKEN
 
-const msg = new Messenger()
-
+// inicializa o slack bot
 const rtm = new RtmClient(token, {
   // logLevel: 'debug'
 })
 
+// escuta as mensagens enviadas para o bot
 rtm.on(RTM_EVENTS.MESSAGE, (data) => {
-  console.log(data)
-  msg.Receive(data.user, data.ts, data.text)
+  let answer = bot.talk(data.text)
+  rtm.sendMessage(answer, data.channel)
 })
 
+// habilita o slack bot
 rtm.start()
